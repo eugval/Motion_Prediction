c7ec8f5749aa155ac8d76937c00f65a7ced4b534
eugval <pe.val@icloud.com>
2018-06-22 19:25:07 +0100

    adding prints

--- src/preprocessing/from_zero_to_hero.py
+++ src/preprocessing/from_zero_to_hero.py
@@ -20,6 +20,7 @@ names = ["Light1", "Light2", "Crossing1", "Crossing2"]
 
 #Run the MaskRcnn
 for name in names:
+    print("dealing with {} ...".format(name))
     #Directory to fetch the raw images
     raw_dir_name = name
 
@@ -40,24 +41,34 @@ for name in names:
     target_folder_consolidated = os.path.join(PROCESSED_PATH, "{}/tracked_images_consolidated/".format(name))
     target_folder_gauss = os.path.join(PROCESSED_PATH, "{}/tracked_images_gauss/".format(name))
 
+    print("Detecting...")
     run_mask_rcnn(detected_file,
                   processed_dir,
                   generate_image_dir_path(raw_dir_name), save_images=True)
 
+    print("Discarding Masks...")
     discard_masks(data_file,tracked_file, small_threshold = 20)
 
+    print("Tracking...")
     track(tracked_file)
 
+    print("Tracking reverse...")
     track(tracked_file, reverse=True)
 
+    print("Consolidating...")
     consolidate_indices(tracked_file)
 
+    print("Resizing...")
     resize_data(tracked_file, resized_file, 256, 314, maintain_ratio=True)
 
+    print("Making gaussian masks...")
     make_gaussian_masks(resized_file)
 
+    print("Making the dataset...")
     make_dataset(resized_file, dataset_file)
 
+    print("Making Visualisation...")
     visualise_tracks(resized_file, target_folder_consolidated, id_idx = 0)
 
+    print("Making Gaussian Visualisation...")
     visualise_gaussians(resized_file,target_folder_gauss)
